231cb0f6b3289e5744ccace13384484381192825
hypernova1 <hypemova@gmail.com>
2024-09-10 14:00:29 +0900

    init

---
+++ src/main/kotlin/org/sam/lms/Application.kt
@@ -0,0 +1,11 @@
+package org.sam.lms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
